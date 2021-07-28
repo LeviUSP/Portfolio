b0f425e6801da41d338784742e8b2d835841ff6c
LeviUSP <levifernandes@usp.br>
2021-07-28 16:39:42 +0200

    Added all styles for the new site

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
 ScrollOut({
-  targets: '.hero, #projects-image, header, .headline'
+  targets: '.hero, #projects-image, header, .headline, .service, .project-card'
 });
+
+
+const projectCards = document.querySelectorAll('.project-card')
+
+projectCards.forEach(element => {
+  element.addEventListener('click', event => {
+    if(element.classList.contains('expanded')){
+      element.classList.remove('expanded')
+    } 
+    else element.classList.add('expanded')
+  })
+});
+
+
+
